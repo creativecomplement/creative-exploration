b95fa56dc4541d0c4a12ea1ac9a46115feea039b
Donald Zullick <donald@creativecomplement.com>
2020-10-26 17:14:24 -0500

    research, notes, implementation to come

---
+++ src/components/Sheets.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+const apiKey = process.env.REACT_APP_SHEETS_KEY;
+
+/* 
+For me: don't forget the .env updates
+
+Note: Dropdown, code missing in tutorial. Looks like I need to install
+react-dropdown (https://www.npmjs.com/package/react-dropdown)
+
+ Going to add some notes here, the tutorial for "Build an Online Retail
+ Dashboard in React" is woefully lacking. I am integrating it into my 
+ project here so already paring it down. I am slightly tempted to re-create
+ it with better/more complete instructions and an open source charting
+ tool. First I will get it working here then maybe work through a new
+ tutorial.
+*/
